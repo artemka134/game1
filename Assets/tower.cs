06e21c2b5c458ef7ca59a807eb3682e5bfae9042
artemka134 <artem.andreanov28@gmail.com>
2024-12-06 17:43:50 +0300

    06.12.24

--- Assets/tower.cs
+++ Assets/tower.cs
@@ -23,6 +23,11 @@ public class tower : MonoBehaviour
 	//		position_zombie.Add(collision.gameObject);
 	//	}
 	//}
+	public void Start()
+	{
+		transform.Find("Canvas/Image/текст").gameObject.GetComponent<TextMeshProUGUI>().text = health + "/" + max_health;
+		transform.Find("Canvas/Image").gameObject.GetComponent<Image>().fillAmount = health / max_health;
+	}
 	public void OnTriggerStay2D(Collider2D other)
 	{
 		if (other.gameObject.tag == "zombie" && position_zombie.Contains(other.gameObject) == false && other.gameObject.GetComponent<movement_zombi>().spawn == true)
