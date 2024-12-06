7ff41f5007ff63af0fcf92688751e446ec5a05d0
artemka134 <artem.andreanov28@gmail.com>
2024-12-06 17:52:13 +0300

    06.12.24.2

--- Assets/movement_zombi.cs
+++ Assets/movement_zombi.cs
@@ -31,17 +31,6 @@ public class movement_zombi : MonoBehaviour
 			transform.position = Vector2.MoveTowards(transform.position, attack_tower.transform.position, speed);
 		}
 	}
-	public void OnTriggerStay2D(Collider2D collision)
-	{
-		if (collision.gameObject.transform.position.y < transform.position.y && collision.gameObject.tag == "zombie")
-		{
-			gameObject.GetComponent<SpriteRenderer>().sortingOrder = 5;
-		}
-		if (collision.gameObject.transform.position.y > transform.position.y && collision.gameObject.tag == "zombie")
-		{
-			gameObject.GetComponent<SpriteRenderer>().sortingOrder = 6;
-		}
-	}
 	public void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.tag == "tower")
